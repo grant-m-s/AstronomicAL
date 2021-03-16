0e12e8cccf75eabadae602f498be6efa0dafa9d2
Grant Stevens <grant-stevens@hotmail.co.uk>
2021-03-16 14:01:41 +0000

    Test saving param assignments
    Former-commit-id: 25241837a553eac585d9a8fc0fa681dcfce51dc4

--- tests/all_test.py
+++ tests/all_test.py
@@ -4,6 +4,7 @@ sys.path.insert(1, os.path.join(sys.path[0], "../"))
 import pandas as pd
 from astronomicAL.dashboard.dashboard import Dashboard
 from astronomicAL.settings.param_assignment import ParameterAssignment
+import astronomicAL.config as config
 
 
 class TestClass:
@@ -20,7 +21,7 @@ class TestSettings:
         data = []
 
         for i in range(100):
-            data.append([i, i, i, i, i])
+            data.append([i, i % 3, i, i, i])
 
         df = pd.DataFrame(data, columns=list("ABCDE"))
 
@@ -175,3 +176,202 @@ class TestSettings:
             "D",
             "E",
         ]
+
+    def test_param_assignment_greater_than_20_labels_dont_proceed(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "E"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        assert paramAssignment.label_strings_param == {}
+
+    def test_param_assignment_less_than_20_labels_proceed(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        assert paramAssignment.label_strings_param != {}
+
+    def test_param_assignment_strings_to_labels_check_save_without_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["strings_to_labels"] == {"0": 0, "1": 1, "2": 2}
+
+    def test_param_assignment_strings_to_labels_check_save_with_some_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment.label_strings_param["0"].value = "string_label_for_0"
+        paramAssignment.label_strings_param["2"].value = "string_label_for_2"
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["strings_to_labels"] == {
+            "string_label_for_0": 0,
+            "1": 1,
+            "string_label_for_2": 2,
+        }
+
+    def test_param_assignment_strings_to_labels_check_save_with_all_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment.label_strings_param["0"].value = "string_label_for_0"
+        paramAssignment.label_strings_param["1"].value = "string_label_for_1"
+        paramAssignment.label_strings_param["2"].value = "string_label_for_2"
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["strings_to_labels"] == {
+            "string_label_for_0": 0,
+            "string_label_for_1": 1,
+            "string_label_for_2": 2,
+        }
+
+    def test_param_assignment_labels_to_strings_check_save_without_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["labels_to_strings"] == {"0": "0", "1": "1", "2": "2"}
+
+    def test_param_assignment_labels_to_strings_check_save_with_some_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment.label_strings_param["0"].value = "string_label_for_0"
+        paramAssignment.label_strings_param["2"].value = "string_label_for_2"
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["labels_to_strings"] == {
+            "0": "string_label_for_0",
+            "1": "1",
+            "2": "string_label_for_2",
+        }
+
+    def test_param_assignment_labels_to_strings_check_save_with_all_inputs(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment.label_strings_param["0"].value = "string_label_for_0"
+        paramAssignment.label_strings_param["1"].value = "string_label_for_1"
+        paramAssignment.label_strings_param["2"].value = "string_label_for_2"
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["labels_to_strings"] == {
+            "0": "string_label_for_0",
+            "1": "string_label_for_1",
+            "2": "string_label_for_2",
+        }
+
+    def test_param_assignment_selected_columns_saved_to_config_correctly(self):
+
+        df = self._create_test_df()
+
+        paramAssignment = ParameterAssignment()
+
+        paramAssignment.update_data(df)
+
+        paramAssignment.id_column = "A"
+        paramAssignment.label_column = "B"
+        paramAssignment.default_x_variable = "C"
+        paramAssignment.default_y_variable = "D"
+
+        paramAssignment._update_labels_cb()
+
+        paramAssignment._confirm_settings_cb(None)
+
+        assert config.settings["id_col"] == "A"
+        assert config.settings["label_col"] == "B"
+        assert config.settings["default_vars"] == ("C", "D")
