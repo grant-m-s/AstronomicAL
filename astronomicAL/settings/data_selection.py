872c59caadd8c5209e8a853b207e07ca858d8b97
Grant Stevens <grant-stevens@hotmail.co.uk>
2021-05-02 01:43:11 +0100

    Add a verify config step to loading a config displaying any errors to user
    Former-commit-id: 5646293b0b7b9148e57e6517e67e1d41331a1522

--- astronomicAL/settings/data_selection.py
+++ astronomicAL/settings/data_selection.py
@@ -1,9 +1,13 @@
 from astronomicAL.utils.optimise import optimise
+from astronomicAL.utils import load_config
 from astropy.table import Table
 from bokeh.models import TextAreaInput
 from bokeh.models.callbacks import CustomJS
+from functools import partial
 
 import astronomicAL.config as config
+import glob
+import json
 import numpy as np
 import pandas as pd
 import panel as pn
@@ -32,7 +36,8 @@ class DataSelection(param.Parameterized):
     """
 
     dataset = param.FileSelector(path="data/*")
-    config_file = param.FileSelector(path="configs/*.json")
+    config_file = param.ObjectSelector(default="", objects=[""])
+    load_config_select = param.ObjectSelector(default="", objects=[""])
 
     load_layout_check = param.Boolean(False, label="Load Custom Configuration?")
 
@@ -43,12 +48,27 @@ class DataSelection(param.Parameterized):
 
         self.mode = mode
         self.src = src
+        self.error_message = ""
 
         self._initialise_widgets()
 
         self.panel_col = pn.Column("Loading...")
 
     def _initialise_widgets(self):
+
+        config_files = self._get_config_files()
+        load_config_options = self._init_load_config_options()
+
+        config_files = [""] + config_files
+        load_config_options = [""] + load_config_options
+
+        self.param.load_config_select.objects = load_config_options
+        self.param.config_file.objects = config_files
+        self.param.load_config_select.default = load_config_options[0]
+        self.param.config_file.default = config_files[0]
+        self.load_config_select = load_config_options[0]
+        self.config_file = config_files[0]
+
         print("CONFIG:")
         print(config.layout_file)
         self.memory_optimisation_check = pn.widgets.Checkbox(
@@ -59,66 +79,95 @@ class DataSelection(param.Parameterized):
             max_width=5,
         )
 
-        if self.mode == "AL":
-            self.load_config_select = pn.widgets.Select(
-                name="How Much Would You Like To Load?",
-                options=[
-                    "Only load layout. Let me choose all my own settings",
-                    "Load all settings but let me train the model from scratch.",
-                    "Load all settings and train model with provided labels.",
-                ],
-                max_width=400,
-            )
-
-        elif self.mode == "Labelling":
-            self.load_config_select = pn.widgets.Select(
-                name="How Much Would You Like To Load?",
-                options=[
-                    "Only load layout. Let me choose all my own settings",
-                    "Load all settings and begin labelling data.",
-                ],
-                max_width=400,
-            )
-
         self.load_data_button = pn.widgets.Button(
             name="Load Data File", max_height=30, margin=(45, 0, 0, 0)
         )
         self.load_data_button.on_click(self._load_data_cb)
 
         self.load_data_button_js = pn.widgets.Button(
-            name="Load Configuration File", max_height=30, margin=(45, 0, 0, 0)
+            name="Load Configuration File",
+            max_height=30,
+            margin=(45, 0, 0, 0),
+            button_type="success",
         )
 
-        self.load_data_button_js.js_on_click(
-            args={"button": self.load_data_button_js},
-            code="""
-                console.log('Reloading...')
-                button.label = 'Loading New Layout - Please Wait, This may take a minute.'
-                button.disabled = true
+        self.load_data_button_js.jscallback(
+            clicks="""
 
-                var i;
-                for (i = 0; i < 40; i++) {
-                  if (i % 3 === 0) {
-                      setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute.'; }, 500*i);
-                   } else if (i % 3 === 1) {
-                      setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute..'; }, 500*i);
-                   } else {
-                      setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute...'; }, 500*i);
-                   }
-                }
+                        console.log('Reloading...')
 
-                setTimeout(() => {  location.reload(); }, 2000);
+                        button.label = 'Loading New Layout - Please Wait, This may take a minute.'
+                        button.disabled = true
 
-                """,
+                        var i;
+                        for (i = 0; i < 40; i++) {
+                          if (i % 3 === 0) {
+                              setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute.'; }, 500*i);
+                           } else if (i % 3 === 1) {
+                              setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute..'; }, 500*i);
+                           } else {
+                              setTimeout(() => {  button.label = 'Loading New Layout - Please Wait, This may take a minute...'; }, 500*i);
+                           }
+                        }
+                        setTimeout(() => {  location.reload(); }, 2000);
+                    """,
+            args=dict(button=self.load_data_button_js),
         )
 
-        self.load_data_button_js.on_click(self._update_layout_file_cb)
+    def _get_config_files(self):
+        files = glob.glob("configs/*.json")
+        return files
 
-    def _update_layout_file_cb(self, event):
+    def _init_load_config_options(self):
+        if self.mode == "AL":
+            options = [
+                "Only load layout. Let me choose all my own settings",
+                "Load all settings but let me train the model from scratch.",
+                "Load all settings and train model with provided labels.",
+            ]
+
+        elif self.mode == "Labelling":
+            options = [
+                "Only load layout. Let me choose all my own settings",
+                "Load all settings and begin labelling data.",
+            ]
+        else:
+            options = []
+
+        return options
+
+    @param.depends("load_config_select", "config_file", watch=True)
+    def _update_layout_file_cb(self):
+
+        if (self.load_config_select == "") or (self.config_file == ""):
+            self.error_message = ""
+            self.load_data_button_js.label = "Select values from dropdown to continue"
+            self.load_data_button_js.disabled = True
+            return
+
         config.layout_file = self.config_file
-        config.settings["config_load_level"] = list(
-            self.load_config_select.options
-        ).index(self.load_config_select.value)
+        config.settings["config_load_level"] = (
+            list(self.param.load_config_select.objects).index(self.load_config_select)
+            - 1
+        )
+
+        with open(config.layout_file) as layout_file:
+            curr_config_file = json.load(layout_file)
+
+        has_error, error_message = load_config.verify_import_config(curr_config_file)
+
+        if has_error:
+            print(f"has error - {error_message}")
+            self.error_message = error_message
+            self.load_data_button_js.label = "Unable to load config"
+            self.load_data_button_js.disabled = True
+        else:
+            print(f"no error - {error_message}")
+            self.error_message = "verified"
+            self.error_message = ""
+            self.load_data_button_js.label = "Load Data"
+            self.load_data_button_js.disabled = False
+
         print(f"Config load level: {config.settings['config_load_level']}")
         print(f"INSIDE CB: {config.layout_file}")
 
@@ -137,11 +186,9 @@ class DataSelection(param.Parameterized):
 
         """
         start = time.time()
-        try:
-            ext = filename[filename.rindex(".") + 1 :]
-            fits_table = Table.read(filename, format=f"{ext}")
-        except:
-            assert False, "Unknown Filetype"
+
+        ext = filename[filename.rindex(".") + 1 :]
+        fits_table = Table.read(filename, format=f"{ext}")
 
         end = time.time()
         print(f"Loading FITS Table {end - start}")
@@ -224,6 +271,10 @@ class DataSelection(param.Parameterized):
         new_df = pd.DataFrame([[0, 0], [0, 0]], columns=["test", "test"])
         self.src.data = dict(new_df)
 
+    def _panel_change_cb(self, attr, old, new):
+        print("PANEL CB RAN")
+        self.panel_col = self.panel()
+
     @param.depends("load_layout_check", watch=True)
     def _panel_cb(self):
         print("PANEL CB RAN")
@@ -239,33 +290,44 @@ class DataSelection(param.Parameterized):
             settings Dashboard.
 
         """
+        if (self.error_message == "") or (self.error_message == "verified"):
+            message = "Welcome!"
+        else:
+            message = pn.pane.Markdown(self.error_message)
+
         if self.load_layout_check:
-            self.panel_col[0] = pn.Column(
-                pn.layout.VSpacer(max_height=10),
-                pn.Row(self.param.load_layout_check, max_height=30),
-                pn.Row(self.param.config_file, max_width=300),
-                pn.Row(self.load_config_select),
-                pn.Row(self.load_data_button_js),
-                pn.layout.VSpacer(),
-                pn.layout.VSpacer(),
-                pn.layout.VSpacer(),
+            self.panel_col[0] = pn.Row(
+                pn.Column(
+                    pn.layout.VSpacer(max_height=10),
+                    pn.Row(self.param.load_layout_check, max_height=30),
+                    pn.Row(self.param.config_file, max_width=300),
+                    pn.Row(self.param.load_config_select),
+                    pn.Row(self.load_data_button_js),
+                    pn.layout.VSpacer(),
+                    pn.layout.VSpacer(),
+                    pn.layout.VSpacer(),
+                ),
+                pn.Row(message),
             )
 
         else:
-            self.panel_col[0] = pn.Column(
-                pn.layout.VSpacer(max_height=10),
-                pn.Row(self.param.load_layout_check, max_height=30),
-                pn.Row(self.param.dataset, max_width=300),
-                pn.Row(
-                    self.memory_optimisation_check,
-                    self._memory_opt_tooltip,
+            self.panel_col[0] = pn.Row(
+                pn.Column(
+                    pn.layout.VSpacer(max_height=10),
+                    pn.Row(self.param.load_layout_check, max_height=30),
+                    pn.Row(self.param.dataset, max_width=300),
+                    pn.Row(
+                        self.memory_optimisation_check,
+                        self._memory_opt_tooltip,
+                        max_width=300,
+                    ),
+                    pn.Row(self.load_data_button, max_width=300),
+                    pn.layout.VSpacer(max_width=300),
+                    pn.layout.VSpacer(max_width=300),
+                    pn.layout.VSpacer(max_width=300),
                     max_width=300,
                 ),
-                pn.Row(self.load_data_button, max_width=300),
-                pn.layout.VSpacer(max_width=300),
-                pn.layout.VSpacer(max_width=300),
-                pn.layout.VSpacer(max_width=300),
-                max_width=300,
+                pn.Row("Welcome!"),
             )
 
         return self.panel_col
