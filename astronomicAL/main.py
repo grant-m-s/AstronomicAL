93b6dd4cd208dffdbcd785e5aceb0a3100ec1c0c
Grant Stevens <grant-stevens@hotmail.co.uk>
2021-05-25 09:57:35 +0100

    feedback to user that config saved on header save layout button

--- astronomicAL/main.py
+++ astronomicAL/main.py
@@ -5,11 +5,13 @@ sys.path.insert(1, os.path.join(sys.path[0], "../"))
 from astronomicAL.utils import save_config, load_config
 from bokeh.models import ColumnDataSource, TextAreaInput
 from functools import partial
+from multiprocessing import Process
 
 import astronomicAL.config as config
 import holoviews as hv
 import pandas as pd
 import panel as pn
+import time
 
 hv.extension("bokeh")
 hv.renderer("bokeh").webgl = True
@@ -25,7 +27,24 @@ if os.path.isfile(config.layout_file):
 else:
     react = load_config.create_default_layout(react)
 
-save_layout_button = pn.widgets.Button(name="Save current layout")
+save_layout_button = pn.widgets.Button(name="Save Current Configuration")
+
+
+def _save_layout_button_rename():
+    save_layout_button.disabled = True
+    save_layout_button.name = (
+        "Configuration saved to configs folder with current timestamp."
+    )
+    time.sleep(3)
+    save_layout_button.name = "Save Current Configuration"
+    save_layout_button.disabled = False
+
+
+def _save_layout_button_cb(event):
+    Process(target=_save_layout_button_rename).start()
+
+
+save_layout_button.on_click(_save_layout_button_cb)
 
 layout_dict = {}
 text_area_input = TextAreaInput(value="")
