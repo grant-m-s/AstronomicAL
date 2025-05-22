e95d319f7f51a9c0c26219a565eb9a476a8f39b6
ivanosaccheo <ivanosaccheo@gmail.com>
2025-05-22 17:28:55 +0100

    Added global shared dictionary to share informations between extension plots. Merged plotting DESI and SDSS functions

---
+++ astronomicAL/extensions/extension_plots_shared.py
@@ -0,0 +1,12 @@
+#### Dictionary with all data sharee between the extensions plots
+
+
+shared_data = {"is_global": True}
+
+# List of keys defined for the global shared_data dictionary
+# is_global : bool,  debug key to see that the dictionary is seen from the dashboards
+# Sparcl_client : object, The client to access NOIR-DataLab spectra
+# DESI_ra : list, list of ra of DESI spectra retrieved
+# DESI_dec : list, list of dec of DESI spectra retrieved
+# Euclid_radius : float, radius of the Euclid cutout
+#
