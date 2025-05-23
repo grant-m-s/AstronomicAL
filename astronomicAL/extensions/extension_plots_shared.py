60d1a92ba9db80b6e81dfe65714b2fc96182508a
ivanosaccheo <ivanosaccheo@gmail.com>
2025-05-23 15:45:55 +0100

    added publish-subscribe pattern to make plots share information

--- astronomicAL/extensions/extension_plots_shared.py
+++ astronomicAL/extensions/extension_plots_shared.py
@@ -3,10 +3,27 @@
 
 shared_data = {"is_global": True}
 
+
 # List of keys defined for the global shared_data dictionary
 # is_global : bool,  debug key to see that the dictionary is seen from the dashboards
 # Sparcl_client : object, The client to access NOIR-DataLab spectra
-# DESI_ra : list, list of ra of DESI spectra retrieved
-# DESI_dec : list, list of dec of DESI spectra retrieved
+# DESI_coordinates : dictionary {"ra" : [ra], "dec" : [dec]}, coordinates of DESI spectra retrieved
+# SDSS_coordinates : dictionary {"ra" : [ra], "dec" : [dec]}, coordinates of SDSS spectra retrieved
 # Euclid_radius : float, radius of the Euclid cutout
-#
+
+subscribers = {}
+
+def subscribe(key, callback):
+    if key not in subscribers:
+        subscribers[key] = []
+    subscribers[key].append(callback)
+ 
+
+def publish(key, value):
+    shared_data[key] = value
+    for callback in subscribers.get(key, []):
+        callback(value)
+
+  
+
+#Trigger function = c = self.src.data.copy() ----> self.src.data = c
