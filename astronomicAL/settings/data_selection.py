e01a7f3e67a80ed0eb5262d40a678d61e387d7ea
Grant Stevens <grant-stevens@hotmail.co.uk>
2026-02-28 09:50:41 +0000

    Small bug fix

--- astronomicAL/settings/data_selection.py
+++ astronomicAL/settings/data_selection.py
@@ -264,7 +264,7 @@ class DataSelection(param.Parameterized):
 
         print(config.settings)
 
-        config.main_df = self.get_dataframe_from_fits_file(self.dataset, config.settings["optimise_data"])
+        config.main_df = self.get_dataframe_from_fits_file(self.dataset)
         self.df = config.main_df
         self.src.data = dict(pd.DataFrame())
 
